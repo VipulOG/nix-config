52cf6732a1a52fa311be5634a52b974b107a54d7
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-06-26 16:16:21 +0530

    update stylix etra config

--- modules/home/misc/stylix/stylix.nix
+++ modules/home/misc/stylix/stylix.nix
@@ -20,67 +20,66 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable (
-    {
-      stylix = {
-        enable = true;
-        image = config.lib.stylix.pixel "base03";
-        base16Scheme = config.custom.common.constants.colorSchemeYaml;
-        polarity = "dark";
-
-        cursor = {
-          package = pkgs.simp1e-cursors;
-          name = "Simp1e";
-          size = 32;
-        };
-
-        fonts = {
-          emoji = {
-            name = "Noto Color Emoji";
-            package = pkgs.noto-fonts-emoji;
-          };
-
-          monospace = {
-            name = "JetBrainsMono Nerd Font";
-            package = pkgs.nerd-fonts.jetbrains-mono;
-          };
-
-          sansSerif = {
-            name = "Inter";
-            package = pkgs.inter;
-          };
-
-          serif = {
-            name = "Merriweather";
-            package = pkgs.merriweather;
-          };
-
-          sizes = {
-            applications = 12;
-            desktop = 12;
-            popups = 12;
-            terminal = 14;
-          };
-        };
-
-        iconTheme = {
+  config = lib.mkIf cfg.enable {
+    stylix = lib.mkMerge [
+      (
+        lib.mkDefault {
           enable = true;
-          package = pkgs.colloid-icon-theme.override {
-            schemeVariants = ["catppuccin"];
-            colorVariants = ["purple"];
-          };
-          light = "Colloid-Purple-Catppuccin-Light";
-          dark = "Colloid-Purple-Catppuccin-Dark";
-        };
+          image = config.lib.stylix.pixel "base03";
+          base16Scheme = config.custom.common.constants.colorSchemeYaml;
+          polarity = "dark";
 
-        opacity = {
-          applications = 1.0;
-          desktop = 1.0;
-          popups = 1.0;
-          terminal = 1.0;
-        };
-      };
-    }
-    # // lib.removeAttrs cfg.extraConfig ["enable"]
-  );
+          cursor = {
+            package = pkgs.simp1e-cursors;
+            name = "Simp1e";
+            size = 32;
+          };
+
+          fonts = {
+            emoji = {
+              name = "Noto Color Emoji";
+              package = pkgs.noto-fonts-emoji;
+            };
+            monospace = {
+              name = "JetBrainsMono Nerd Font";
+              package = pkgs.nerd-fonts.jetbrains-mono;
+            };
+            sansSerif = {
+              name = "Inter";
+              package = pkgs.inter;
+            };
+            serif = {
+              name = "Merriweather";
+              package = pkgs.merriweather;
+            };
+            sizes = {
+              applications = 12;
+              desktop = 12;
+              popups = 12;
+              terminal = 14;
+            };
+          };
+
+          iconTheme = {
+            enable = true;
+            package = pkgs.colloid-icon-theme.override {
+              schemeVariants = ["catppuccin"];
+              colorVariants = ["purple"];
+            };
+            light = "Colloid-Purple-Catppuccin-Light";
+            dark = "Colloid-Purple-Catppuccin-Dark";
+          };
+
+          opacity = {
+            applications = 1.0;
+            desktop = 1.0;
+            popups = 1.0;
+            terminal = 1.0;
+          };
+        }
+      )
+
+      (lib.removeAttrs cfg.extraConfig ["enable"])
+    ];
+  };
 }
