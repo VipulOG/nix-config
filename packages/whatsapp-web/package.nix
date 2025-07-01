9abd029c3d869adfe02bd148cec2256b93b70429
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-07-01 13:48:51 +0530

    update whatsapp package

--- packages/whatsapp-web/package.nix
+++ packages/whatsapp-web/package.nix
@@ -10,14 +10,9 @@ pkgs.stdenv.mkDerivation {
   nativeBuildInputs = [pkgs.makeWrapper];
 
   installPhase = ''
-    mkdir -p $out/bin
+    install -Dm755 ${./whatsapp-web.sh} $out/bin/whatsapp-web
 
-    makeWrapper ${pkgs.brave}/bin/brave $out/bin/whatsapp-web \
-      --add-flags "--app=https://web.whatsapp.com" \
-      --add-flags "--user-data-dir=$HOME/.whatsapp-web-data" \
-      --add-flags "--no-first-run" \
-      --add-flags "--class=com.whatsapp.web" \
-      --add-flags "--disable-features=TranslateUI"
+    wrapProgram $out/bin/whatsapp-web --prefix PATH : ${pkgs.brave}/bin
 
     install -Dm644 ${./whatsapp-web.desktop} \
       $out/share/applications/whatsapp-web.desktop
@@ -27,7 +22,7 @@ pkgs.stdenv.mkDerivation {
   '';
 
   meta = with lib; {
-    description = "Brave wrapper for WhatsApp Web in app mode";
+    description = "Standalone WhatsApp Web launcher using Brave Browser’s app mode";
     homepage = "https://web.whatsapp.com";
     platforms = platforms.linux;
   };
