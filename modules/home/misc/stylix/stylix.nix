16a700275043a0afb27f15bfe18ccc22ae3d9f15
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-07-01 16:14:05 +0530

    format stylix

--- modules/home/misc/stylix/stylix.nix
+++ modules/home/misc/stylix/stylix.nix
@@ -22,57 +22,55 @@ in {
 
   config = lib.mkIf cfg.enable {
     stylix = lib.mkMerge [
-      (
-        lib.mkDefault {
+      (lib.mkDefault {
+        enable = true;
+        image = config.lib.stylix.pixel "base03";
+        base16Scheme = config.custom.common.vars.colorScheme.yaml;
+        polarity = "dark";
+        inherit (config.custom.common.vars) cursor;
+
+        fonts = {
+          emoji = {
+            name = "Noto Color Emoji";
+            package = pkgs.noto-fonts-emoji;
+          };
+          monospace = {
+            name = "JetBrainsMono Nerd Font";
+            package = pkgs.nerd-fonts.jetbrains-mono;
+          };
+          sansSerif = {
+            name = "Inter";
+            package = pkgs.inter;
+          };
+          serif = {
+            name = "Merriweather";
+            package = pkgs.merriweather;
+          };
+          sizes = {
+            applications = 12;
+            desktop = 12;
+            popups = 12;
+            terminal = 14;
+          };
+        };
+
+        iconTheme = {
           enable = true;
-          image = config.lib.stylix.pixel "base03";
-          base16Scheme = config.custom.common.vars.colorScheme.yaml;
-          polarity = "dark";
-          inherit (config.custom.common.vars) cursor;
-
-          fonts = {
-            emoji = {
-              name = "Noto Color Emoji";
-              package = pkgs.noto-fonts-emoji;
-            };
-            monospace = {
-              name = "JetBrainsMono Nerd Font";
-              package = pkgs.nerd-fonts.jetbrains-mono;
-            };
-            sansSerif = {
-              name = "Inter";
-              package = pkgs.inter;
-            };
-            serif = {
-              name = "Merriweather";
-              package = pkgs.merriweather;
-            };
-            sizes = {
-              applications = 12;
-              desktop = 12;
-              popups = 12;
-              terminal = 14;
-            };
+          package = pkgs.colloid-icon-theme.override {
+            schemeVariants = ["default"];
+            colorVariants = ["default"];
           };
+          light = "Colloid-Light";
+          dark = "Colloid-Dark";
+        };
 
-          iconTheme = {
-            enable = true;
-            package = pkgs.colloid-icon-theme.override {
-              schemeVariants = ["default"];
-              colorVariants = ["default"];
-            };
-            light = "Colloid-Light";
-            dark = "Colloid-Dark";
-          };
-
-          opacity = {
-            applications = 1.0;
-            desktop = 1.0;
-            popups = 1.0;
-            terminal = 1.0;
-          };
-        }
-      )
+        opacity = {
+          applications = 1.0;
+          desktop = 1.0;
+          popups = 1.0;
+          terminal = 1.0;
+        };
+      })
 
       (lib.removeAttrs cfg.extraConfig ["enable"])
     ];
