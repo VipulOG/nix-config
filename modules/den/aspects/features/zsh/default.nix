97031283ddc580e02131c0a92a61109125a97654
vipulog <90324465+vipulog@users.noreply.github.com>
2026-07-03 11:09:10 +0530

    feat(zsh): add welcome plugin

--- modules/den/aspects/features/zsh/default.nix
+++ modules/den/aspects/features/zsh/default.nix
@@ -5,11 +5,25 @@
     };
 
     homeManager = {
+      lib,
+      pkgs,
+      ...
+    }: {
       programs.zsh = {
         enable = true;
         enableCompletion = true;
         autosuggestion.enable = true;
         fastSyntaxHighlighting.enable = true;
+
+        plugins = [
+          {
+            name = "welcome";
+            src = pkgs.writeTextDir "welcome.plugin.zsh" ''
+              [[ -o interactive ]] || return
+              ${lib.getExe pkgs.fastfetch} -l "none"
+            '';
+          }
+        ];
       };
     };
   };
