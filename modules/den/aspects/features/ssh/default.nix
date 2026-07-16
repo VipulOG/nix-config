24dba87a182d394d9698279d57ff37bf21e28304
vipulog <90324465+vipulog@users.noreply.github.com>
2026-07-16 14:56:50 +0530

    fix(ssh): set IdentityFile with correct user_host key order

--- modules/den/aspects/features/ssh/default.nix
+++ modules/den/aspects/features/ssh/default.nix
@@ -1,11 +1,17 @@
 {
   den.aspects.ssh = {
     homeManager = {
+      host,
+      user,
+      ...
+    }: {
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
 
         settings."*" = {
+          IdentityFile = "~/.ssh/id_${user.name}_${host.name}";
+          IdentitiesOnly = "yes";
           AddKeysToAgent = "yes";
           ForwardAgent = "yes";
         };
