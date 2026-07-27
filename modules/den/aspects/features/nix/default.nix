c902e1ad3b73788a41bccf6b7f7a63cb5812313e
vipulog <90324465+vipulog@users.noreply.github.com>
2026-07-28 00:25:37 +0530

    feat(nix): add redirector and nvf binary cache

--- modules/den/aspects/features/nix/default.nix
+++ modules/den/aspects/features/nix/default.nix
@@ -23,11 +23,15 @@
           substituters = [
             "https://cache.nixos.org/"
             "https://nix-community.cachix.org"
+            "https://redirector.cachix.org"
+            "https://nvf.cachix.org"
           ];
 
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "redirector.cachix.org-1:lx9grKUxrkiq/H1qkIV/oEgRB9SmYGD2Yg37fHs6TlE="
+            "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
           ];
         };
 
